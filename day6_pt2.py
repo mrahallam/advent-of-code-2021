7ec26558c53cef8bb6c5d4d900588db2d0cb4dc3
mrahallam <95549825+mrahallam@users.noreply.github.com>
2021-12-06 15:55:26 +0000

    Update day6_pt2.py

--- day6_pt2.py
+++ day6_pt2.py
@@ -7,24 +7,13 @@ for line in input22:
 from collections import Counter
 count = Counter(start_fish)
 
-count[0] = 0
-count[6] = 0
-count[7] = 0
-count[8] = 0
-
 for i in range(256):
     state = []
     for i in range(9):
         state.append(count.get(i,0))
-        
-    count[0] = state[1]
-    count[1] = state[2]
-    count[2] = state[3]
-    count[3] = state[4]
-    count[4] = state[5]
-    count[5] = state[6]
-    count[6] = state[7]+state[0]
-    count[7] = state[8]
+    for i in range(9):
+        count[i] = count[i+1]
+    count[6] = count[6]+state[0]
     count[8] = state[0]
 
 for key, value in sorted(count.items(), key=lambda x: x[0]):
