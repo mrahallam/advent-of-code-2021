c73117e5f95ad0b4104d954090e1188d88a1fc41
mrahallam <95549825+mrahallam@users.noreply.github.com>
2021-12-10 13:03:52 +0000

    Update day10.py

--- 2021/day10.py
+++ 2021/day10.py
@@ -7,17 +7,14 @@ with open(file_name) as file:
     lines = file.readlines()
     lines = [list((line.strip())) for line in lines]
 
-legal_pairs = [('(',')'),('[',']'),('{','}'),('<','>')]
-opening_characters = ['(','[','{','<']
-closing_characters = [')',']','}','>']
-points = {')':3,']':57,'}':1197,'>':25137}
-score_multiplier = {'(':1,'[':2,'{':3,'<':4}
+legal_pairs = [('(',')',3,1),('[',']',57,2),('{','}',1197,3),('<','>',25137,4)]
+points = dict([pair[1:2]+pair[2:3] for pair in legal_pairs])
+score_multiplier = dict([pair[0:1]+pair[3:4] for pair in legal_pairs])
 
-illegals = []
 total_syntax_error = 0
 
 def check_for_closed_pair(prev_char,char):
-    if (prev_char,char) in legal_pairs:
+    if (prev_char,char) in [pair[0:2] for pair in legal_pairs]:
         return True
 
 for line in lines[:]:
@@ -31,22 +28,18 @@ for line in lines[:]:
         if check_for_closed_pair(prev_char,char):
             check_line = check_line[:-2]
         if len(check_line) > 1:
-            if check_line[-1] in closing_characters:
-                #print(f'Line is {"".join(line)}, {check_line}, corrput')
-                illegals.append(check_line[-1])
+            if check_line[-1] in [pair[1] for pair in [pair[0:2] for pair in legal_pairs]]:
+                total_syntax_error += points[check_line[-1]]
                 lines.remove(line)
                 break
-
-for i in illegals:
-    total_syntax_error += points[i]
 
 print(f'The total syntax error score is: {total_syntax_error}')
 
 total_scores = []
 
-for line in lines[:]:
+for line in lines:
     check_line = line
-    while set(check_line).intersection(set(closing_characters)):
+    while set(check_line).intersection(set([pair[1] for pair in [pair[0:2] for pair in legal_pairs]])):
         for i in range(len(check_line)-1,0,-1):
             if check_for_closed_pair(check_line[i-1],check_line[i]):
                 check_line = check_line[0: i-1] + check_line[i + 1::]
